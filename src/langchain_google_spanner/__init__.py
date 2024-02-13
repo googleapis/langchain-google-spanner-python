e354491619f3f7c400d8f1c827d365ebb553937a
gauravpurohit <gauravpurohit@google.com>
2024-02-13 05:58:01 +0000

    feat: Adding init file to export VectorStore

--- src/langchain_google_spanner/__init__.py
+++ src/langchain_google_spanner/__init__.py
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from langchain_google_spanner.vector_store import SpannerVectorStore
+
+__all__ = ["SpannerVectorStore"]
