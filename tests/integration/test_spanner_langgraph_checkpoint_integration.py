35bda9e8a7623cc7cdbd6467edde73e82d43c4db
Yeesian Ng <ysian@google.com>
2024-11-05 00:46:35 +0000

    test: initial implementation of LangGraph Checkpointer integration tests

--- tests/integration/test_spanner_langgraph_checkpoint_integration.py
+++ tests/integration/test_spanner_langgraph_checkpoint_integration.py
@@ -11,3 +11,68 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import os
+import uuid
+
+import pytest  # noqa
+
+from langchain_google_spanner import SpannerCheckpointSaver
+
+project_id = os.environ["PROJECT_ID"]
+instance_id = os.environ["INSTANCE_ID"]
+
+_TEST_CHECKPOINT = {
+    "v": 1,
+    "ts": "2024-07-31T20:14:19.804150+00:00",
+    "id": "1ef4f797-8335-6428-8001-8a1503f9b875",
+    "channel_values": {
+        "my_key": "meow",
+        "node": "node"
+    },
+    "channel_versions": {
+        "__start__": 2,
+        "my_key": 3,
+        "start:node": 3,
+        "node": 3
+    },
+    "versions_seen": {
+        "__input__": {},
+        "__start__": {
+        "__start__": 1
+        },
+        "node": {
+        "start:node": 2
+        }
+    },
+    "pending_sends": [],
+}
+_TEST_WRITE_CONFIG = {"configurable": {"thread_id": "1", "checkpoint_ns": ""}}
+_TEST_READ_CONFIG = {"configurable": {"thread_id": "1"}}
+
+@pytest.fixture(scope="module")
+def setup():
+    for env in ["GOOGLE_DATABASE"]: #, "PG_DATABASE"]:
+        checkpointer = SpannerCheckpointSaver(
+            instance_id=instance_id,
+            database_id=os.environ.get(env),
+            project_id=project_id,
+        )
+        with checkpointer.cursor() as cur:
+            cur.executemany(
+                "DROP TABLE IF EXISTS %s",
+                ["checkpoints", "checkpoint_writes"],
+            )
+        checkpointer.setup()
+
+def test_chat_message_history(setup) -> None:
+    for env in ["GOOGLE_DATABASE"]: #, "PG_DATABASE"]:
+        checkpointer = SpannerCheckpointSaver(
+            instance_id=instance_id,
+            database_id=os.environ.get(env),
+            project_id=project_id,
+        )
+        checkpointer.put(_TEST_WRITE_CONFIG, _TEST_CHECKPOINT, {}, {})
+        checkpoint = checkpointer.get(_TEST_READ_CONFIG)
+        assert checkpoint == _TEST_CHECKPOINT
+        assert len(list(checkpointer.list(_TEST_READ_CONFIG))) == 1
