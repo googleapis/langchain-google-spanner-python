6cc31cb40dbec99f32b251383c96f91a9c487d33
Mingtian Yin <mtyin@google.com>
2025-02-19 18:07:29 -0800

    Format code

--- src/langchain_google_spanner/graph_store.py
+++ src/langchain_google_spanner/graph_store.py
@@ -901,10 +901,12 @@ class SpannerGraphSchema(object):
         Returns:
           str: a string representation of the graph schema.
         """
-        properties = CaseInsensitiveDict({
-            k: TypeUtility.spanner_type_to_schema_str(v)
-            for k, v in self.properties.items()
-        })
+        properties = CaseInsensitiveDict(
+            {
+                k: TypeUtility.spanner_type_to_schema_str(v)
+                for k, v in self.properties.items()
+            }
+        )
         return json.dumps(
             {
                 "Name of graph": self.graph_name,
